2ad1a4582d3d92007ec90e9c462daf360bd6ee50
HelloWorld-n <gajica.b@gmail.com>
2022-05-26 14:25:34 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -40,10 +40,22 @@ def unique(collection):
 	return True
 
 if __name__ == "__main__":
-	thing = [0, 2, 4, 5, 6, 7]	
+	thing = []
+	amountOfDigits = None
+	while amountOfDigits == None or not 2 < amountOfDigits < 10:
+		amountOfDigits = int(input("Enter amount of digits: "))
+	
+	sizes = [None, None]
+	while sizes[0] == None or not 0 < sizes[0] < amountOfDigits - 1:
+		sizes[0] = int(input("Enter sizes[0]: "))
+	while sizes[1] == None or not 0 < sizes[1] < amountOfDigits - sizes[0]:
+		sizes[1] = int(input("Enter sizes[1]: "))
+
+	for i in range(amountOfDigits):
+		thing.append(int(input(f"Enter digit[{i}]: ")))	
 	try:
 		for combination in combine(thing, len(thing), needsUnique = True):
-			if embasy(combination[:2]) * embasy(combination[2:3]) == embasy(combination[3:]):
+			if embasy(combination[:sizes[0]]) * embasy(combination[sizes[0]:(sizes[0] + sizes[1])]) == embasy(combination[sizes[0] + sizes[1]:]):
 				print(combination)
 	except IndexError:
 		print("Complete!")
